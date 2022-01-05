7e7ab97b3932384776a13ae4a1d98431f1db5f56
James Kibugu <48236990+Kibugu@users.noreply.github.com>
2022-01-05 15:27:01 +0300

    installing more packages

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\admin\Documents\Pricing_Report\last_expense\venv
+set VIRTUAL_ENV=c:\Users\admin\Documents\Pricing_Report\last_expense\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
